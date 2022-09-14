19ad4b1275067cdd7e104e2f70be65a1639948cb
Renan Borges <renanvcb@gmail.com>
2022-09-13 22:40:37 -0300

    aula 02

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,42 @@
+import {
+  FlatList,
+  View,
+  Image
+} from 'react-native'
+
+import logoImg from '../../assets/logo-nlw-esports.png'
+
+import { Heading } from '../../components/Heading'
+import { GameCard } from '../../components/GameCard'
+
+import { GAMES } from '../../utils/games'
+
+import { styles } from './styles'
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={logoImg}
+        style={styles.logo}
+      />
+      <Heading
+        title='Encontre seu duo!'
+        subtitle='Selecione o game que deseja jogar...'
+      />
+
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GameCard
+            data={item}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+      />
+    </View>
+  );
+}
