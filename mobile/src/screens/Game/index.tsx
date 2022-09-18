49f58f371fe20e966cdb2617acd53edd434b36a7
Renan Borges <renanvcb@gmail.com>
2022-09-17 21:55:21 -0300

    Aula 05 - Mobile

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -9,6 +9,7 @@ import { GameParams } from '../../@types/navigation'
 import { Background } from '../../components/Background'
 import { Heading } from '../../components/Heading'
 import { DuoCard, DuoCardProps } from '../../components/DuoCard'
+import { DuoMatch } from '../../components/DuoMatch'
 
 import { styles } from './styles'
 import { THEME } from '../../theme'
@@ -17,12 +18,19 @@ import logoImg from '../../assets/logo-nlw-esports.png'
 
 export function Game() {
   const [duos, setDuos] = useState<DuoCardProps[]>([])
+  const [discordDuoSelected, setDiscordDuoSelected] = useState('')
   const navigation = useNavigation()
   const route = useRoute()
   const game = route.params as GameParams
 
   function handleGoBack() {
     navigation.goBack()
+  }
+
+  async function getDiscordUser(adId: string) {
+    fetch(`http://192.168.1.6:3333/ads/${adId}/discord`)
+      .then(response => response.json())
+      .then(data => setDiscordDuoSelected(data.discord))
   }
 
   useEffect(() => {
@@ -69,7 +77,7 @@ export function Game() {
           renderItem={({ item }) => (
             <DuoCard
               data={item}
-              onConnect={() => { }}
+              onConnect={() => getDiscordUser(item.id)}
             />
           )}
           horizontal
@@ -85,6 +93,11 @@ export function Game() {
           }
         />
 
+        <DuoMatch
+          visible={discordDuoSelected.length > 0}
+          discord={discordDuoSelected}
+          onClose={() => setDiscordDuoSelected('')}
+        />
       </SafeAreaView>
     </Background>
   );
