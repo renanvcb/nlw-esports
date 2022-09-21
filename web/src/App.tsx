beace0cd9d3344cdc5719ca71d3f191000a83bad
Renan Borges <renanvcb@gmail.com>
2022-09-21 00:14:44 -0300

    slider set with no arrows

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 import * as Dialog from '@radix-ui/react-dialog'
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
 
 import { CreateAdBanner } from "./components/CreateAdBanner"
 import { GameBanner } from "./components/GameBanner"
@@ -21,12 +23,51 @@ interface Game {
 
 function App() {
   const [games, setGames] = useState<Game[]>([])
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    slides: {
+      perView: 5,
+      spacing: 15,
+    },
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel)
+    },
+    created() {
+      setLoaded(true)
+    },
+  })
 
   useEffect(() => {
     axios('http://localhost:3333/games').then(response => {
       setGames(response.data)
     })
   }, [])
+
+  function Arrow(props: {
+    disabled: boolean
+    left?: boolean
+    onClick: (e: any) => void
+  }) {
+    const disabeld = props.disabled ? " arrow--disabled" : ""
+    return (
+      <svg
+        onClick={props.onClick}
+        className={`arrow ${props.left ? "arrow--left" : "arrow--right"
+          } ${disabeld}`}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        {props.left && (
+          <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+        )}
+        {!props.left && (
+          <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+        )}
+      </svg>
+    )
+  }
 
   return (
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
@@ -36,15 +77,17 @@ function App() {
         Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
       </h1>
 
-      <div className='grid grid-cols-6 gap-6 mt-16'>
+      <div ref={sliderRef} className="keen-slider">
         {games.map(game => {
           return (
-            <GameBanner
-              key={game.id}
-              bannerUrl={game.bannerUrl}
-              title={game.title}
-              adsCount={game._count.ads}
-            />
+            <div className="keen-slider__slide">
+              <GameBanner
+                key={game.id}
+                bannerUrl={game.bannerUrl}
+                title={game.title}
+                adsCount={game._count.ads}
+              />
+            </div>
           )
         })}
       </div>
